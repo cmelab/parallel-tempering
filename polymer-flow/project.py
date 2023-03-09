544c16865ed37846abbcd7b4b92d1dc0c7b8d57d
chrisjones4 <chrisjones4@u.boisestate.edu>
2023-03-09 14:04:14 -0700

    finish adding the restart logic

--- polymer-flow/project.py
+++ polymer-flow/project.py
@@ -92,17 +92,14 @@ def copy_trajectory(job, fname):
     shutil.copyfile(job.fn("trajectory.gsd"), job.fn("fname"))
 
 
-def load_pickle_objects(job, system_file, ff_file):
+def load_pickle_ff(job, ff_file):
     """
     Load hoomd snapshot and list of hoomd forces
     from pickle files in a job's workspace.
     """
-    with open(job.fn(system_file), "rb") as f:
-        snap = pickle.load(f)
     with open(job.fn(ff_file), "rb") as f:
         hoomd_ff = pickle.load(f)
-
-    return snap, hoomd_ff
+    return hoomd_ff
 
 
 @directives(executable="python -u")
@@ -121,60 +118,63 @@ def sample(job):
         print("----------------------")
 
         # Setting up the system
-        # TODO: Fix restart logic, polymers repo can start a sim
-        # directly from a snapshot/gsd and a list of hoomd ff objects
-        # Right now, working with pickle files to save snapshot and list of ff
-        #restart = job.isfile("restart.gsd")
-        molecule_obj = getattr(hoomd_polymers.molecules, job.sp.molecule)
-        ff_obj = getattr(hoomd_polymers.forcefields, job.sp.forcefield)
-        system_obj = getattr(hoomd_polymers.systems, job.sp.system)
-        system = system_obj(
-                molecule=molecule_obj,
-                n_mols=job.sp.n_chains,
-                chain_lengths=job.sp.chain_lengths,
-                density=job.sp.density,
-                mol_kwargs=job.sp.molecule_kwargs,
-                packing_expand_factor=3,
-        )
-        print("----------------------")
-        print("System generated...")
-        print("----------------------")
-        print("Applying the forcefield...")
-        print("----------------------")
-        system.apply_forcefield(
-                forcefield=ff_obj(),
-                remove_hydrogens=job.sp.remove_hydrogens,
-                make_charge_neutral=True
-        )
+        if job.isfile("restart.gsd"): # Initializing from a restart.gsd
+            with gsd.hoomd.open(job.fn("restart.gsd")) as traj:
+                init_snap = traj[0]
+            hoomd_ff = load_picke_ff(job, "forcefield.picke")
+        else: # No restart, generate the system and apply a FF
+            molecule_obj = getattr(hoomd_polymers.molecules, job.sp.molecule)
+            ff_obj = getattr(hoomd_polymers.forcefields, job.sp.forcefield)
+            system_obj = getattr(hoomd_polymers.systems, job.sp.system)
+            system = system_obj(
+                    molecule=molecule_obj,
+                    n_mols=job.sp.n_chains,
+                    chain_lengths=job.sp.chain_lengths,
+                    density=job.sp.density,
+                    mol_kwargs=job.sp.molecule_kwargs,
+                    packing_expand_factor=3,
+            )
+            print("----------------------")
+            print("System generated...")
+            print("----------------------")
+            print("Applying the forcefield...")
+            print("----------------------")
+            system.apply_forcefield(
+                    forcefield=ff_obj(),
+                    remove_hydrogens=job.sp.remove_hydrogens,
+                    make_charge_neutral=True
+            )
+            init_snap = system.hoomd_snapshot
+            hoomd_ff = system.hoomd_forcefield
+
+            job.doc.gsd_write_frequency = int(
+                    job.doc.total_steps / job.sp.num_gsd_frames
+            )
+            job.doc.log_write_frequency = int(
+                    job.doc.total_steps / job.sp.num_data_logs
+            )
+
         # Set up stuff to initialize a Simulation
-        job.doc.gsd_write_frequency = int(
-                job.doc.total_steps / job.sp.num_gsd_frames
-        )
-        job.doc.log_write_frequency = int(
-                job.doc.total_steps / job.sp.num_data_logs
-        )
         sim = Simulation(
-                initial_state=system.hoomd_snapshot,
-                forcefield=system.hoomd_forcefield,
+                initial_state=init_snap,
+                forcefield=system.hoomd_ff,
                 gsd_write_freq=job.doc.gsd_write_frequency,
                 log_write_freq=job.doc.log_write_frequency,
         )
-        print("SIM DEVICE:")
-        print(sim.device)
-        print("----------------------")
-        sim.pickle_forcefield()
+        if not job.fn("forcefield.pickle"): # Pickle FF for future runs
+            sim.pickle_forcefield()
         if job.sp.e_factor != 1:
             print("Scaling LJ epsilon values...")
             sim.adjust_epsilon(scale_by=job.sp.e_factor)
+
         print("----------------------")
         print("Starting simulation...")
         print("----------------------")
-        job.doc.target_box_reduced = (
-                system.target_box*10/system.reference_values.distance
-        )
-
         # Run shrink simulation
         if not job.doc.ran_shrink:
+            job.doc.target_box_reduced = (
+                    system.target_box*10/system.reference_values.distance
+            )
             sim.run_update_volume(
                     final_box_lengths=job.doc.target_box_reduced,
                     n_steps=job.sp.shrink_steps,
@@ -189,7 +189,7 @@ def sample(job):
             print("Shrink simulation finished...")
             print("----------------------")
 
-        # Run NVT simulation
+        # Run NVT simulation. # Starting here for jobs after swaps
         sim.run_NVT(
                 n_steps=job.sp.n_steps, kT=job.sp.kT, tau_kt=job.sp.tau_kt
         )
@@ -203,6 +203,7 @@ def sample(job):
         else:
             copy_trajectory(job, fname=f"trajectory_{job.doc.current_run}.gsd")
         job.doc["timestep"].append(sim.timestep)
+
         job.doc["current_run"] += 1
         sim.reset_system() # What exactly is this func doing, how to implement with polymer sims
         job.doc["done"] = True
